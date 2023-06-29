4336ce2a372791a7447ec48e81fd59c12db554e0
Andre <andre.fabian.melo@gmail.com>
2023-06-28 22:56:49 -0300

    feat: Add tooth map icons

---
+++ odontoapp-web/src/assets/toothMap/t21.tsx
@@ -0,0 +1,26 @@
+import { SVGAttributes } from "react";
+
+export function T21(props: SVGAttributes<SVGElement>) {
+  return (
+    <svg
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      viewBox="0 0 58 162"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path
+        strokeWidth="4"
+        d="M42.895 52.156c-.605-7.344 5.395-50.652 0-50.152s-12.157 21.283-16.92 36.11c-4.25 13.229-2.448 15.275-6.975 34.89-4.5 19.5-14 31-14.5 38s-6 43 2 45.5c4.825 1.508 13.53 2.476 13.53 2.476h21.493s10.06.501 11.89-3.01c1.83-3.51 3.659-16.55 1.83-31.595-1.83-15.046-4.65-18.224-6.86-30.091C46.808 85.83 44.623 69 44.623 69s-1.437-13.314-1.728-16.845Z"
+      />
+      <path
+        strokeWidth="2"
+        d="M50.5 104S36.96 99.332 28 99.5c-8.195.153-20.5 4.5-20.5 4.5"
+      />
+    </svg>
+  );
+}
--- odontoapp-web/src/utils/getIcon.tsx
+++ odontoapp-web/src/utils/getIcon.tsx
@@ -3,10 +3,15 @@ import { BellIcon } from "@/assets/icons/bell";
 import { CalendarDaysIcon } from "@/assets/icons/calendarDays";
 import { ChatBubbleLeftEllipsisIcon } from "@/assets/icons/chatBubbleLeftEllipsis";
 import { CheckIcon } from "@/assets/icons/check";
+import { CheckCircleIcon } from "@/assets/icons/checkCircle";
 import { ChevronLeftIcon } from "@/assets/icons/chevronLeft";
 import { ChevronRightIcon } from "@/assets/icons/chevronRight";
 import { DocumentTextIcon } from "@/assets/icons/document";
+import { EnvelopeIcon } from "@/assets/icons/envelope";
 import { MagnifyingGlassIcon } from "@/assets/icons/magnifier";
+import { MapPinIcon } from "@/assets/icons/mapPin";
+import { PencilIcon } from "@/assets/icons/pencil";
+import { PhoneIcon } from "@/assets/icons/phone";
 import { PlusCircleIcon } from "@/assets/icons/plusCircle";
 import { ShoppingCartIcon } from "@/assets/icons/shoppingCart";
 import { Squares2x2Icon } from "@/assets/icons/squares2x2";
@@ -20,6 +25,39 @@ import { VideoCameraIcon } from "@/assets/icons/videoCamera";
 import { WrenchScrewdriverIcon } from "@/assets/icons/wrenchScrewdriver";
 import { XCircleIcon } from "@/assets/icons/xcircle";
 import { XMarkIcon } from "@/assets/icons/xmark";
+import { T11 } from "@/assets/toothMap/t11";
+import { T12 } from "@/assets/toothMap/t12";
+import { T13 } from "@/assets/toothMap/t13";
+import { T14 } from "@/assets/toothMap/t14";
+import { T21 } from "@/assets/toothMap/t21";
+import { T22 } from "@/assets/toothMap/t22";
+import { T23 } from "@/assets/toothMap/t23";
+import { T24 } from "@/assets/toothMap/t24";
+import { T15 } from "@/assets/toothMap/t15";
+import { T16 } from "@/assets/toothMap/t16";
+import { T17 } from "@/assets/toothMap/t17";
+import { T18 } from "@/assets/toothMap/t18";
+import { T25 } from "@/assets/toothMap/t25";
+import { T26 } from "@/assets/toothMap/t26";
+import { T27 } from "@/assets/toothMap/t27";
+import { T28 } from "@/assets/toothMap/t28";
+import { T31 } from "@/assets/toothMap/t31";
+import { T41 } from "@/assets/toothMap/t41";
+import { T32 } from "@/assets/toothMap/t32";
+import { T42 } from "@/assets/toothMap/t42";
+import { T43 } from "@/assets/toothMap/t43";
+import { T33 } from "@/assets/toothMap/t33";
+import { T34 } from "@/assets/toothMap/t34";
+import { T44 } from "@/assets/toothMap/t44";
+import { T35 } from "@/assets/toothMap/t35";
+import { T36 } from "@/assets/toothMap/t36";
+import { T37 } from "@/assets/toothMap/t37";
+import { T38 } from "@/assets/toothMap/t38";
+import { T45 } from "@/assets/toothMap/t45";
+import { T46 } from "@/assets/toothMap/t46";
+import { T47 } from "@/assets/toothMap/t47";
+import { T48 } from "@/assets/toothMap/t48";
+import { BuildingOfficeIcon } from "@/assets/icons/buildingOffice";
 
 type Props = {
   name: string;
@@ -69,10 +107,86 @@ export function getIcon({ name, ...rest }: Props) {
       return <BellIcon {...rest} />;
     case "check":
       return <CheckIcon {...rest} />;
+    case "check-circle":
+      return <CheckCircleIcon {...rest} />;
     case "xmark":
       return <XMarkIcon {...rest} />;
     case "user":
       return <UserIcon {...rest} />;
+    case "phone":
+      return <PhoneIcon {...rest} />;
+    case "envelope":
+      return <EnvelopeIcon {...rest} />;
+    case "map-pin":
+      return <MapPinIcon {...rest} />;
+    case "pencil":
+      return <PencilIcon {...rest} />;
+    case "building-office":
+      return <BuildingOfficeIcon {...rest} />;
+    case "t11":
+      return <T11 {...rest} />;
+    case "t12":
+      return <T12 {...rest} />;
+    case "t13":
+      return <T13 {...rest} />;
+    case "t14":
+      return <T14 {...rest} />;
+    case "t15":
+      return <T15 {...rest} />;
+    case "t16":
+      return <T16 {...rest} />;
+    case "t17":
+      return <T17 {...rest} />;
+    case "t18":
+      return <T18 {...rest} />;
+    case "t21":
+      return <T21 {...rest} />;
+    case "t22":
+      return <T22 {...rest} />;
+    case "t23":
+      return <T23 {...rest} />;
+    case "t24":
+      return <T24 {...rest} />;
+    case "t25":
+      return <T25 {...rest} />;
+    case "t26":
+      return <T26 {...rest} />;
+    case "t27":
+      return <T27 {...rest} />;
+    case "t28":
+      return <T28 {...rest} />;
+    case "t31":
+      return <T31 {...rest} />;
+    case "t32":
+      return <T32 {...rest} />;
+    case "t33":
+      return <T33 {...rest} />;
+    case "t34":
+      return <T34 {...rest} />;
+    case "t35":
+      return <T35 {...rest} />;
+    case "t36":
+      return <T36 {...rest} />;
+    case "t37":
+      return <T37 {...rest} />;
+    case "t38":
+      return <T38 {...rest} />;
+    case "t41":
+      return <T41 {...rest} />;
+    case "t42":
+      return <T42 {...rest} />;
+    case "t43":
+      return <T43 {...rest} />;
+    case "t44":
+      return <T44 {...rest} />;
+    case "t45":
+      return <T45 {...rest} />;
+    case "t46":
+      return <T46 {...rest} />;
+    case "t47":
+      return <T47 {...rest} />;
+    case "t48":
+      return <T48 {...rest} />;
     default:
       break;
   }
