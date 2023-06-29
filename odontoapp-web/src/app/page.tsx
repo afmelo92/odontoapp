8052ea7e6f178be9e194576d68771ffa051a69fc
Andre <andre.fabian.melo@gmail.com>
2023-06-29 01:43:07 -0300

    fix: adjustments in main page style proportions"

--- odontoapp-web/src/app/page.tsx
+++ odontoapp-web/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Dashboard() {
   return (
     <>
       <h1 className="font-bold text-2xl">Welcome, Andre Melo</h1>
-      <div className="w-full h-full rounded-lg shadow-gray-500 grid grid-rows-[128px_1fr_1fr_1fr_1fr_1fr_1fr] gap-4">
+      <div className="w-full h-full rounded-lg shadow-gray-500 grid grid-rows-[128px_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr] gap-4">
         <AdBanner />
         <Statistics />
         <SecondaryStats />
