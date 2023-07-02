bcc499730221b47430f3d7e75b00c9d55cdd3860
Andre <andre.fabian.melo@gmail.com>
2023-07-02 11:33:44 -0300

    fix: Update logger data to file only in production

--- odontoapp-api/src/utils/logger.ts
+++ odontoapp-api/src/utils/logger.ts
@@ -1,20 +1,15 @@
 import { format, transports, createLogger } from 'winston';
-const { combine, timestamp, json, prettyPrint, simple } = format;
+const { combine, timestamp, json, prettyPrint } = format;
 
 const logger = createLogger({
   format: combine(json(), timestamp(), prettyPrint()),
-  transports: [
-    new transports.Console(),
-    new transports.File({
-      filename: `./logs/default.log`,
-    }),
-  ],
+  transports: [new transports.Console()],
 });
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'production') {
   logger.add(
-    new transports.Console({
-      format: simple(),
+    new transports.File({
+      filename: `./logs/default.log`,
     })
   );
 }
