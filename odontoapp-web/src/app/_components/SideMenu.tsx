4400a3224595280553c3f622166d2c63d0f1fc90
Andre <andre.fabian.melo@gmail.com>
2023-07-31 21:35:53 -0300

    fix: update pathname match function to regex strategy

--- odontoapp-web/src/app/_components/SideMenu.tsx
+++ odontoapp-web/src/app/_components/SideMenu.tsx
@@ -15,24 +15,30 @@ const SideMenu: React.FC = () => {
       id="side-menu"
       className="col-span-1 flex flex-col items-center py-6 gap-1"
     >
-      {userData?.menu?.map((item) => (
-        <Link
-          key={item.uid}
-          href={`/${item.href}`}
-          className={`group p-4 rounded-lg ${
-            pathname === item.href ? "bg-blue-100" : ""
-          }`}
-          title={item.title}
-        >
-          {getIcon({
-            name: item.icon,
-            className: `w-6 h-w-6 transition-colors ${
-              pathname === item.href ? "stroke-blue-500" : "stroke-gray-500"
-            } group-hover:stroke-blue-500`,
-            strokeWidth: 2,
-          })}
-        </Link>
-      ))}
+      {userData?.menu?.map((item) => {
+        const regexp = new RegExp(`^/${item.href}(?:\/.*)?$`, "i");
+
+        return (
+          <Link
+            key={item.uid}
+            href={`/${item.href}`}
+            className={`group p-4 rounded-lg ${
+              pathname?.match(regexp)?.length ? "bg-blue-100" : ""
+            }`}
+            title={item.title}
+          >
+            {getIcon({
+              name: item.icon,
+              className: `w-6 h-w-6 transition-colors ${
+                pathname?.match(regexp)?.length
+                  ? "stroke-blue-500"
+                  : "stroke-gray-500"
+              } group-hover:stroke-blue-500`,
+              strokeWidth: 2,
+            })}
+          </Link>
+        );
+      })}
     </aside>
   );
 };
